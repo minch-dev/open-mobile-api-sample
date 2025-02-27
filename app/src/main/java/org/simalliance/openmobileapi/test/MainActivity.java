12af9ffaf21002166dc58500f8acd7d035ec1fbe
minch-dev <minchenko.dev@gmail.com>
2025-02-28 02:35:36 +0300

    moved res folder where it now belongs, also still grasping the concept of lambda as executor

--- app/src/main/java/org/simalliance/openmobileapi/test/MainActivity.java
+++ app/src/main/java/org/simalliance/openmobileapi/test/MainActivity.java
@@ -62,7 +62,7 @@ public class MainActivity extends Activity {
 
 		
 		SEServiceCallback callback = new SEServiceCallback();
-		new SEService(this, callback);
+		new SEService(this,{ it.run() },callback);
 	}
 	/**
 	 * Interface to receive call-backs when the service is connected. If the
